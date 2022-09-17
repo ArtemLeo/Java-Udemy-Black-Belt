8d257d1977cd85c8da72b9eefe831ed775b92fb5
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-09-17 04:24:25 +0300

    Update lesson_01.java

--- src/task_10_Generics_Example/lesson_01.java
+++ src/task_10_Generics_Example/lesson_01.java
@@ -2,10 +2,10 @@ package task_10_Generics_Example;
 
 public class lesson_01 {
     public static void main(String[] args) {
-        Schoolar schoolar1 = new Schoolar("Ivan",13);
-        Schoolar schoolar2 = new Schoolar("Anna",15);
-        Schoolar schoolar3 = new Schoolar("Ilya",12);
-        Schoolar schoolar4 = new Schoolar("Sergey",14);
+        Schoolar scholar1 = new Schoolar("Artem",13);
+        Schoolar scholar2 = new Schoolar("Anna",15);
+        Schoolar scholar3 = new Schoolar("Ilya",12);
+        Schoolar scholar4 = new Schoolar("Sergey",14);
 
         Student student1 = new Student("Nikolay",20);
         Student student2 = new Student("Mariya",21);
@@ -13,20 +13,20 @@ public class lesson_01 {
         Employee employee1 = new Employee("Artem",32);
         Employee employee2 = new Employee("Mariya",30);
 
-        Team<Schoolar> schoolarTeam = new Team<>("Drakoni!");
-        Team<Schoolar> schoolarTeam1 = new Team<>("MoveOn!");
+        Team<Schoolar> scholarTeam = new Team<>("Dragoni!");
+        Team<Schoolar> scholarTeam1 = new Team<>("MoveOn!");
         Team<Student> studentTeam = new Team<>("GetUp!");
         Team<Employee> employeeTeam = new Team<>("ComeOn!");
 
-        schoolarTeam.addNewParticipant(schoolar1);
-        schoolarTeam.addNewParticipant(schoolar2);
-        schoolarTeam1.addNewParticipant(schoolar3);
-        schoolarTeam1.addNewParticipant(schoolar4);
+        scholarTeam.addNewParticipant(scholar1);
+        scholarTeam.addNewParticipant(scholar2);
+        scholarTeam1.addNewParticipant(scholar3);
+        scholarTeam1.addNewParticipant(scholar4);
         studentTeam.addNewParticipant(student1);
         studentTeam.addNewParticipant(student2);
         employeeTeam.addNewParticipant(employee1);
         employeeTeam.addNewParticipant(employee2);
         System.out.println(" ");
-        schoolarTeam.playWith(schoolarTeam1);
+        scholarTeam.playWith(scholarTeam1);
     }
 }
