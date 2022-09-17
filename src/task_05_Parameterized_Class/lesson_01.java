1148f011423d7e88bfd1e6da314b89bfb247ba16
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-09-17 04:07:41 +0300

    Update lesson_01.java

--- src/task_05_Parameterized_Class/lesson_01.java
+++ src/task_05_Parameterized_Class/lesson_01.java
@@ -5,10 +5,12 @@ public class lesson_01 {
         Info<String> info1 = new Info<>("Hello");
         System.out.println(info1);
         String s = info1.getValue();
+        System.out.println(s);
 
         Info<Integer> info2 = new Info<>(18);
         System.out.println(info2);
         Integer i = info2.getValue();
+        System.out.println(i);
     }
 }
 
