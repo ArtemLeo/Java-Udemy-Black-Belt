183aaf0d0e647a413c4c597a3c3e2570c0427594
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-09-17 04:37:32 +0300

    Update lesson_01.java

--- src/task_10_Generics_Example/lesson_01.java
+++ src/task_10_Generics_Example/lesson_01.java
@@ -2,10 +2,10 @@ package task_10_Generics_Example;
 
 public class lesson_01 {
     public static void main(String[] args) {
-        Schoolar scholar1 = new Schoolar("Artem",13);
-        Schoolar scholar2 = new Schoolar("Anna",15);
-        Schoolar scholar3 = new Schoolar("Ilya",12);
-        Schoolar scholar4 = new Schoolar("Sergey",14);
+        Scholar scholar1 = new Scholar("Artem",13);
+        Scholar scholar2 = new Scholar("Anna",15);
+        Scholar scholar3 = new Scholar("Ilya",12);
+        Scholar scholar4 = new Scholar("Sergey",14);
 
         Student student1 = new Student("Nikolay",20);
         Student student2 = new Student("Mariya",21);
@@ -13,8 +13,8 @@ public class lesson_01 {
         Employee employee1 = new Employee("Artem",32);
         Employee employee2 = new Employee("Mariya",30);
 
-        Team<Schoolar> scholarTeam = new Team<>("Dragoni!");
-        Team<Schoolar> scholarTeam1 = new Team<>("MoveOn!");
+        Team<Scholar> scholarTeam = new Team<>("Dragoni!");
+        Team<Scholar> scholarTeam1 = new Team<>("MoveOn!");
         Team<Student> studentTeam = new Team<>("GetUp!");
         Team<Employee> employeeTeam = new Team<>("ComeOn!");
 
