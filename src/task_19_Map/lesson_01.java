58bcc5255efc55fb0d644b2cd1bed36bf7548cdc
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-09-18 04:53:43 +0300

    Update lesson_01.java

--- src/task_19_Map/lesson_01.java
+++ src/task_19_Map/lesson_01.java
@@ -7,7 +7,7 @@ public class lesson_01 {
     public static void main(String[] args) {
         Map<Integer, String> map1 = new HashMap<>();
 
-        // put - добавление элементов в Map;
+        // put - РґРѕР±Р°РІР»РµРЅРёРµ СЌР»РµРјРµРЅС‚РѕРІ РІ Map;
         map1.put(22455, "Tom");
         map1.put(98374564, "Anna");
         map1.put(5395, "Den");
@@ -16,27 +16,27 @@ public class lesson_01 {
         map1.put(9483722, null);
         System.out.println(map1);
 
-        // putIfAbsent - добавить элемент, если такой отсутствует (с такими параметрами)
+        // putIfAbsent - РґРѕР±Р°РІРёС‚СЊ СЌР»РµРјРµРЅС‚, РµСЃР»Рё С‚Р°РєРѕР№ РѕС‚СЃСѓС‚СЃС‚РІСѓРµС‚ (СЃ С‚Р°РєРёРјРё РїР°СЂР°РјРµС‚СЂР°РјРё);
         map1.putIfAbsent(5395, "Artem");
         System.out.println(map1);
 
-        // get - находит/выводит на экран элемент по ключу;
+        // get - РЅР°С…РѕРґРёС‚/РІС‹РІРѕРґРёС‚ РЅР° СЌРєСЂР°РЅ СЌР»РµРјРµРЅС‚ РїРѕ РєР»СЋС‡Сѓ;
         System.out.println(map1.get(5395));
 
-        // remove - удаление элемента по ключу;
+        // remove - СѓРґР°Р»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РїРѕ РєР»СЋС‡Сѓ;
         map1.remove(948556);
         System.out.println(map1);
 
-        // containsValue - проверка на наличие элемента по значению;
+        // containsValue - РїСЂРѕРІРµСЂРєР° РЅР° РЅР°Р»РёС‡РёРµ СЌР»РµРјРµРЅС‚Р° РїРѕ Р·РЅР°С‡РµРЅРёСЋ;
         System.out.println(map1.containsValue("Den"));
 
-        // containsKey - проверка на наличие элемента по ключу;
+        // containsKey - РїСЂРѕРІРµСЂРєР° РЅР° РЅР°Р»РёС‡РёРµ СЌР»РµРјРµРЅС‚Р° РїРѕ РєР»СЋС‡Сѓ;
         System.out.println(map1.containsKey(837463));
 
-        // keySet - находит/выводит на экран все ключи;
+        // keySet - РЅР°С…РѕРґРёС‚/РІС‹РІРѕРґРёС‚ РЅР° СЌРєСЂР°РЅ РІСЃРµ РєР»СЋС‡Рё;
         System.out.println(map1.keySet());
 
-        // values - находит/выводит на экран все значения;
+        // values - РЅР°С…РѕРґРёС‚/РІС‹РІРѕРґРёС‚ РЅР° СЌРєСЂР°РЅ РІСЃРµ Р·РЅР°С‡РµРЅРёСЏ;
         System.out.println(map1.values());
 
         Map<String, String> map2 = new HashMap<>();
